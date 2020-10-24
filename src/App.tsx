528119a63784d05bef19afc74500eaf81643591f
lyhhh <17610603221@163.com>
2020-10-24 11:04:42 +0800

    完成搭建router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Pay from './views/Pay';
+import Income from './views/Income';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path='/pay'>
+						<Pay/>
+					</Route>
+					<Route exact path='/income'>
+						<Income/>
+					</Route>
+					<Route exact path='/statistics'>
+						<Statistics/>
+					</Route>
+					<Redirect exact from="/" to="/pay"/>
+					<Route path="*">
+						<NoMatch/>
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
