a1b93104f836303c9da0301db63696ebd25832ba
lyhhh <17610603221@163.com>
2020-10-24 14:40:17 +0800

    完成首页样式

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import Icon from './Icon';
 
 const BookWrapper = styled.div`
 	width: 320px;
 	position: absolute;
-	margin-top: 20px;
 	left: 50%;
 	transform: translateX(-160px);
 	display: flex;
@@ -23,11 +23,24 @@ margin: 10px 0;
 `;
 
 const Title = styled.div`
-color: #999;
+	color: #999;
+	display: flex;
+	align-items: center;
+	> .icon {
+		width: 1.5em;
+		height: 1.5em;
+		margin-right: 1em;
+	}
 `;
 const Money = styled.div`
 font-size: 1.5em;
 margin: 10px 0;
+	> div {
+		margin-top: 0.5em;
+		color: #999;
+		font-style: italic;
+		font-size: 0.5em;
+	}
 `;
 
 const BookNumber = styled.div`
@@ -40,10 +53,12 @@ const Book = () => {
 		<BookWrapper>
 			<BookItem>
 				<Title>
-					旅行
+					<Icon name="travel" className="icon"/>
+					<span>旅行</span>
 				</Title>
 				<Money>
 					￥ 12584
+					<div>总支出</div>
 				</Money>
 				<BookNumber>
 					8 条记录
@@ -51,48 +66,44 @@ const Book = () => {
 			</BookItem>
 			<BookItem>
 				<Title>
-					旅行
+					<Icon name="travel" className="icon"/>
+					<span>旅行</span>
 				</Title>
 				<Money>
 					￥ 12584
+					<div>总支出</div>
 				</Money>
 				<BookNumber>
-					4 条记录
+					8 条记录
 				</BookNumber>
 			</BookItem>
 			<BookItem>
 				<Title>
-					旅行
+					<Icon name="travel" className="icon"/>
+					<span>旅行</span>
 				</Title>
 				<Money>
 					￥ 12584
+					<div>总支出</div>
 				</Money>
 				<BookNumber>
-					4 条记录
+					8 条记录
 				</BookNumber>
 			</BookItem>
 			<BookItem>
 				<Title>
-					旅行
+					<Icon name="travel" className="icon"/>
+					<span>旅行</span>
 				</Title>
 				<Money>
 					￥ 12584
+					<div>总支出</div>
 				</Money>
 				<BookNumber>
-					4 条记录
+					8 条记录
 				</BookNumber>
 			</BookItem>
-			<BookItem>
-				<Title>
-					旅行
-				</Title>
-				<Money>
-					￥ 12584
-				</Money>
-				<BookNumber>
-					4 条记录
-				</BookNumber>
-			</BookItem>
+
 		</BookWrapper>
 	);
 };
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,25 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 import Book from '../components/Book';
 import Button from '../components/Button';
+import Icon from '../components/Icon';
 
 const Wrapper = styled.div`
 	color: white;
 	position: relative;
 	background: black;
 	height: 100vh;
-	> h1 {
+	> .title {
+	display: flex;
+	align-items: center;
+	width: 320px;
+	margin: auto;
+	> span {
+		font-size: 2em;
 		padding: 15px 0;
-		width: 320px;
-		margin: auto;
+	}
+	> .icon {
+	width: 3em;
+	height: 3em;
+	margin-left: 5px;
+	transform: translateY(-4px);
+	}
 	}
 `;
 
 const Home = () => {
 	return (
 		<Wrapper>
-			<h1>葡萄记账</h1>
+			<div className="title">
+				<span>葡萄记账</span>
+				<Icon name="grap" className="icon"/>
+			</div>
 			<Book/>
-			<Button content={"+ 添加账本"}/>
+			<Button content={'+ 添加账本'}/>
 		</Wrapper>
 	);
 };
